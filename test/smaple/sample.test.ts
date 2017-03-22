09f0106a50a445c36e900cccc8e8dfdca273380e
cotttpan <cotttpan@gmail.com>
2017-03-22 18:47:37 +0900

    chore(test): mocha -> ava
    close #1

--- test/smaple/sample.test.ts
+++
@@ -1,8 +0,0 @@
-import * as assert from 'power-assert';
-import add from './../../src/index';
-
-describe('sample', function () {
-    it('test', function () {
-        assert(add(1, 1) === 2);
-    });
-});
